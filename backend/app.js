d14e9d02d6173d04f6739128e5b26acc8c369b05
Brian Mason <brian.kw.mason@gmail.com>
2022-01-22 08:08:54 -0500

    added security

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const { check, validationResult } = require("express-validator");
 const app = express();
 
@@ -11,23 +15,34 @@ const User = require("./models/User");
 const Event = require("./models/Event");
 
 // Middleware
-// app.use(bodyParser.json());
+//Secures HTTP headers
+app.use(helmet());
+//Stops XSS attacks
+app.use(xss());
+
+//Stops oversending to routes
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 min
+    max: 100,
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("Conntected to db");
+  console.log("Conntected to db");
 });
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
 
 app.get("/api", (req, res) => {});
@@ -38,42 +53,42 @@ app.get("/api", (req, res) => {});
 // });
 
 app.post(
-    "/api/event",
-    [
-        check("title", "Title is required").not().isEmpty(),
-        check("time", "Time is required").not().isEmpty(),
-        check("location", "Location is required").not().isEmpty(),
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-            });
-        }
-
-        const { title, time, location, description } = req.body;
-        const eventFields = {
-            title: title,
-            author: req.user.id,
-            time,
-            location,
-        };
-
-        if (description) profileFields.description = description;
-
-        try {
-            let event = new Event(eventFields);
-
-            await event.save();
-            res.json(profile);
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send("Server Error");
-        }
+  "/api/event",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("time", "Time is required").not().isEmpty(),
+    check("location", "Location is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
+
+    const { title, time, location, description } = req.body;
+    const eventFields = {
+      title: title,
+      author: req.user.id,
+      time,
+      location,
+    };
+
+    if (description) profileFields.description = description;
+
+    try {
+      let event = new Event(eventFields);
+
+      await event.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
 );
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  events: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "events",
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    events: [
-        {
-            type: mongoose.Schema.ObjectId,
-            ref: "events",
-        },
-    ],
+  ],
 });
 
 module.exports = mongoose.model("user", userSchema);
