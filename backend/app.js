7b687a1f81a041183ef1d33d0fad355038c98c51
Brian Mason <brian.kw.mason@gmail.com>
2022-01-22 09:45:46 -0500

    user login and register

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const app = express();
 
@@ -19,6 +20,7 @@ const Event = require("./models/Event");
 app.use(helmet());
 //Stops XSS attacks
 app.use(xss());
+app.use(express.urlencoded({ extended: false }));
 
 //Stops oversending to routes
 app.use(
@@ -29,7 +31,7 @@ app.use(
 );
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Conntected to db");
+  console.log("Connected to db");
 });
 
 app.use((req, res, next) => {
@@ -45,12 +47,52 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page");
+});
 
-// app.post("/register", async (req, res) => {
-//     console.log(req.body);
-//     res.json({ status: "OK" });
-// });
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ email: username }).lean();
+  if (await bcrypt.compare(password, user.password)) {
+    //Username password combination successful
+    const token = jwt.sign(
+      { id: user._id, username: user.email },
+      process.env.JWT_SECRET
+    ); //Publicly visible do not put important stuff here
+    return res.json({ status: "error", data: token });
+  }
+  if (!user) {
+    return res.json({ status: "error", error: "Invalid username/password" });
+  }
+  res.json({ status: "error", error: "Invalid username/password" });
+});
+
+app.post("/api/register", async (req, res) => {
+  const { name, username, password: plainTextPassword } = req.body;
+  const password = await bcrypt.hash(plainTextPassword, 10);
+
+  try {
+    const response = await User.create({
+      name: name,
+      email: username,
+      password: password,
+    });
+    console.log("User created successfully", response);
+  } catch (error) {
+    if (error.code === 11000) {
+      //Duplicate key error
+      return res.json({ status: "Error", error: "Username already in use" });
+    }
+    throw error;
+  }
+
+  res.send(req.body);
+});
+
+app.get("/api/register", (req, res) => {
+  res.send("register page");
+});
 
 app.post(
   "/api/event",
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,38 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  events: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "events",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      minlength: 3,
+      maxlength: 50,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        ,
+        "Please provide a valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [6, "Password too short"],
+    },
+    events: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "events",
+      },
+    ],
+  },
+  { collection: "users" }
+);
 
 module.exports = mongoose.model("user", userSchema);
